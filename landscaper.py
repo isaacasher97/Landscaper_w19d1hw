4a454984ac85bf6e4a67cc323acd7241f7abad55
isaacasher97 <isaac.asher97@gmail.com>
2023-09-01 16:17:22 -0400

    user can buy + use push lawnmower to cut grass

--- landscaper.py
+++ landscaper.py
@@ -1,6 +1,7 @@
 #players starting amount
 money = 0
 has_scissors = False
+has_push_lawnmower = False
 
 #Define function to cut lawn using teeth
 def cut_lawn_teeth():
@@ -14,6 +15,13 @@ def cut_lawn_scissors():
     money += 5
     print("You spent the day cutting lawns with rusty scissors and earned $5.")
 
+# Define funtion to cut the lawn using old push lawnmower
+def cut_lawn_with_pushmower():
+    global money
+    money += 50
+    print("You spent the day cutting lawns with an old-timey push lawnmower and earned $50.")
+
+
 # Define a function to buy rusty scissors
 def buy_scissors():
     global money, has_scissors
@@ -26,6 +34,20 @@ def buy_scissors():
     else:
         print("You don't have enough money to buy scissors.")
 
+# Define a function to buy an old-timey push lawnmower
+def buy_push_lawnmower():
+    global money, has_push_lawnmower
+    if money >= 25 and not has_push_lawnmower and has_scissors:
+        money -= 25
+        has_push_lawnmower = True
+        print("You bought an old-timey push lawnmower.")
+    elif has_push_lawnmower:
+        print("You already have an old-timey push lawnmower.")
+    elif not has_scissors:
+        print("You can't buy a lawnmower without rusty scissors. Buy scissors first.")
+    else:
+        print("You don't have enough money to buy a lawnmower.")
+
 
 # Main game loop
 while True:
@@ -33,8 +55,11 @@ while True:
     print("What do you want to do?")
     print("1. Cut the lawn with your teeth")
     print("2. Cut the lawn with rusty scissors")
-    print("3. Buy rusty scissors")
-    print("4. Quit")
+    print("3. Cut the lawn with an old-timey push lawnmower")
+    print("4. Buy rusty scissors")
+    print("5. Buy an old-timey push lawnmower")
+    print("6. Quit")
+
 
     choice = input("Enter your choice: ")
 
@@ -47,7 +72,14 @@ while True:
         else:
             print("You don't have rusty scissors. Buy them first.")
     elif choice == '3':
-        buy_scissors()
+        if has_push_lawnmower:
+            cut_lawn_with_pushmower()
+        else:
+            print("You don't have an old-timey push lawnmower. Buy it first.")
     if choice == '4':
-        print("Thanks for playing landscaper!")
+        buy_scissors()
+    if choice == '5':
+        buy_push_lawnmower()
+    if choice == '6':
+        print("Thanks for playing!")
         break
